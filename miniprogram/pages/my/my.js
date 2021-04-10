ae8d4a9ff52f01ae5915c7f7a7e9488e9e3f65e4
muaXiaoNing <1159480285@qq.com>
2021-04-10 20:21:31 +0800

    construct my

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -1,11 +1,17 @@
 // pages/my/my.js
+let app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {
+      nickName: '登录/注册',
+      avatarUrl: 'https://pics1.baidu.com/feed/0eb30f2442a7d9337e411e9fbfef831b71f00199.png?token=4218cef735e1a3373678adf78f49aece'
+    },
+    orderNumber: 0,
+    isLogin: false
   },
 
   /**
@@ -26,7 +32,24 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log(app.globalData.userInfo)
+    if (app.globalData.userInfo) {
+      this.setData({
+        isLogin: true,
+        userInfo: app.globalData.userInfo
+      })
+    }
+  },
 
+    /**
+   * 跳转到登录界面
+   */
+  goToLogin: function () {
+    if (!this.data.isLogin) {
+      wx.navigateTo({
+        url: '/pages/index/index'
+      });
+    }
   },
 
   /**
