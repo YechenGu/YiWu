5e1d80e3539a678ace89c41cf67eae159224af58
muaXiaoNing <1159480285@qq.com>
2021-04-10 18:10:07 +0800

    construct good

---
+++ miniprogram/pages/good/good.js
@@ -0,0 +1,36 @@
+// pages/good/good.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 举报
+   */
+  report(){
+
+  },
+
+  /**
+   * 底部栏功能
+   */
+  collect(){
+
+  },
+
+  share(){
+
+  },
+
+  exchange(){
+
+  },
+
+  contact(){
+
+  },
+})
