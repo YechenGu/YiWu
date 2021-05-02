da4c1d3470ba0e885e1dc175e557458fd72984ef
muaXiaoNing <1159480285@qq.com>
2021-05-02 23:54:21 +0800

    refine score

--- miniprogram/pages/score/score.js
+++ miniprogram/pages/score/score.js
@@ -1,66 +1,42 @@
-// pages/score/score.js
+const db = wx.cloud.database()
+const _ = db.command
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    score: '',
+    openid: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let that = this
+    wx.cloud.callFunction({
+      name: 'getOpenid',
+      complete: res => {
+        console.log('云函数获取到的openid:', res.result.openid)
+        var openid = res.result.openid;
+        that.setData({
+          openid: openid
+        })
+        db.collection('score')
+          .doc(openid)
+          .get()
+          .then(res => {
+            console.log(res)
+            that.setData({
+            })
+          })
+          .catch(err=>{
+            console.err
+          })
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
